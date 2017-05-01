f5bd6f42dbfaf15ef57e9431e3450f877b44ab9a
yasuhiro hashimoto <yasuhiro.hashimoro.gssi@gmail.com>
2017-05-01 15:20:40 +0900

    テストコードの削除

--- TimeAlter/ViewController.swift
+++ TimeAlter/ViewController.swift
@@ -12,11 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let time:Time = Time(hpd: 26);
-        var hour: Int;
-        var minute: Int;
-        var second: Int;
-        (hour, minute, second) = time.calcNowTime()
-        NSLog("nowTime is \(hour):\(minute):\(second)");
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
